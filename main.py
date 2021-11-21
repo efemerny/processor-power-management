9b11e0664a114ab98670315603a9858e81b201cf
Rasim <rra17@tpu.ru>
2021-11-21 23:06:45 +0700

    Пробная рабочая версия сделана

--- main.py
+++ main.py
@@ -1,88 +1,91 @@
-import sqlite3
 import os
+import sys
+import design
+import ctypes
+import psutil
 import subprocess
-import sys  # sys нужен для передачи argv в QApplication
-import design  # Это наш конвертированный файл дизайна
 from PyQt5 import QtWidgets
+from darktheme.widget_template import DarkPalette
 
 
-class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
+# Получение информации с консоли
+def get_console_info():
+    # Кодировка консоли
+    l_encoding = ctypes.windll.kernel32.GetConsoleOutputCP()
+    # Вызов нужной команды
+    l_call = subprocess.Popen(["powercfg", "/query",
+                               "381b4222-f694-41f0-9685-ff5bb260df2e",
+                               "54533251-82be-4824-96c1-47b60b740d00",
+                               "be337238-0d82-4146-a960-4f3749d470c7"],
+                              stdout=subprocess.PIPE,
+                              stderr=subprocess.PIPE,
+                              text=True,
+                              encoding=str(l_encoding))
+
+    l_output, l_errors = l_call.communicate()
+
+    # Разделяем по строкам
+    l_console_list = l_output.splitlines()
+
+    # Парсим строку, убираем лишние пробелы
+    for i, val in enumerate(l_console_list):
+        l_console_list[i] = list(filter(None, val.split(' ')))
+
+    # Убираем оставшиеся пустые значения списка
+    l_console_list = [value for value in l_console_list if value != []]
+
+    # Две последние строки записываем в новую переменную
+    l_parameters = l_console_list[-2:]
+    l_charger = l_parameters[0]
+    # l_battery = l_parameters[1] - данный параметр по идее не нужен, но пусть будет:)
+
+    return l_charger[-1][-1]
+
+
+def set_power_parameter(p_charger):
+    os.system(f"Powercfg -setacvalueindex scheme_current sub_processor PERFBOOSTMODE {p_charger}")
+    os.system("Powercfg -setactive scheme_current")
+
+
+class PBMapp(QtWidgets.QMainWindow, design.Ui_MainWindow):
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        self.pushButton.clicked.connect(self.turn_boost_on)
-        self.pushButton_2.clicked.connect(self.turn_boost_off)
-        if not os.path.exists("my-test.db"):
-            self.db_usage()
-        else:
-            self.check_status()
+        self.pushButton.clicked.connect(self.set_boost_on)
+        self.pushButton_2.clicked.connect(self.set_boost_off)
+        self.check_setting_status()
 
-    def db_usage(self):
-        connection = sqlite3.connect("my-test.db")
-        crsr = connection.cursor()
-        sql_command = """
-                CREATE TABLE CHECKER(
-                    id INTEGER NOT NULL
-                ); """
-        crsr.execute(sql_command)
-        sql_command = 'INSERT INTO CHECKER values (0)'
-        crsr.execute(sql_command)
-        connection.commit()
-        self.label_3.setText("off")
-        self.label_3.setStyleSheet("color: red")
-        connection.close()
-
-    def turn_boost_on(self):
-        self.set_status(1)
-        os.system("Powercfg -setacvalueindex scheme_current sub_processor PERFBOOSTMODE 1")
-        os.system("Powercfg -setactive scheme_current")
-        os.startfile("C:\Program Files\Ryzen Controller\Ryzen Controller.exe")
-        # тут надо вызывать команду для получения текущего статуса питания проца
-        #call = subprocess.run(["powercfg", "/query", "381b4222-f694-41f0-9685-ff5bb260df2e", "54533251-82be-4824-96c1-47b60b740d00", "be337238-0d82-4146-a960-4f3749d470c7"], )
-        self.check_status()
-        self.close()
-
-    def turn_boost_off(self):
-        self.set_status(0)
-        os.system("Powercfg -setacvalueindex scheme_current sub_processor PERFBOOSTMODE 0")
-        os.system("Powercfg -setactive scheme_current")
-        self.check_status()
-        self.close()
-
-    def set_status(self, p):
-        connection = sqlite3.connect("my-test.db")
-        crsr = connection.cursor()
-        zapr = "UPDATE CHECKER SET id={0}".format(p)
-        crsr.execute(zapr)
-        connection.commit()
-        select = 'SELECT id FROM CHECKER'
-        crsr.execute(select)
-        status = crsr.fetchone()
-        connection.close()
-
-    def check_status(self):
-        connection = sqlite3.connect("my-test.db")
-        crsr = connection.cursor()
-        select = 'SELECT id FROM CHECKER'
-        crsr.execute(select)
-        status = crsr.fetchone()
-        if status[0] == 1:
-            self.label_3.setText("on")
-            self.label_3.setStyleSheet("color: green")
-            connection.close()
-        elif status[0] == 0:
+    def check_setting_status(self):
+        param = get_console_info()
+        if param == '0':
             self.label_3.setText("off")
             self.label_3.setStyleSheet("color: red")
-            connection.close()
+        elif param == '1':
+            self.label_3.setText("on")
+            self.label_3.setStyleSheet("color: green")
+
+    def set_boost_on(self):
+        set_power_parameter(1)
+        os.startfile("C:\Program Files\Ryzen Controller\Ryzen Controller.exe")
+        os.startfile("C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe")
+        self.check_setting_status()
+
+    def set_boost_off(self):
+        set_power_parameter(0)
+        for process in (process for process in psutil.process_iter() if
+                        process.name() == "Ryzen Controller.exe"): process.kill()
+        self.check_setting_status()
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication\
-    window = ExampleApp()  # Создаём объект класса ExampleApp
+    app.setPalette(DarkPalette())
+    window = PBMapp()
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
 
 
-if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
-    main()  # то запускаем функцию main()
+if __name__ == '__main__':
+    main()
